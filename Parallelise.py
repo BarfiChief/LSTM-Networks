57b5ab9dea0729a5d87c3a86d33f4552f2b77186
Yashvardhan Singh <51024639+ys145@users.noreply.github.com>
2020-05-02 19:17:16 +0530

    Code to split task between CPU cores

--- Parallelise.py
+++ Parallelise.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from keras import backend as K
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
-NUM_PARALLEL_EXEC_UNITS = 2 // Set this equal to the number of cores in your processor
+NUM_PARALLEL_EXEC_UNITS = 2 # Set this equal to the number of cores in your processor
 config = tf.ConfigProto(intra_op_parallelism_threads=NUM_PARALLEL_EXEC_UNITS, inter_op_parallelism_threads=2,
                        allow_soft_placement=True, device_count={'CPU': NUM_PARALLEL_EXEC_UNITS})
 session = tf.Session(config=config)
