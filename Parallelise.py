f41cda1985218ef1098f1dae1c1d0f30d7ab158a
Yashvardhan Singh <51024639+ys145@users.noreply.github.com>
2020-05-02 19:14:38 +0530

    Code to split task between CPU cores

---
+++ Parallelise.py
@@ -0,0 +1,15 @@
+# Code to parallelise and speed up processing
+import os
+import tensorflow as tf
+from keras import backend as K
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+NUM_PARALLEL_EXEC_UNITS = 2
+config = tf.ConfigProto(intra_op_parallelism_threads=NUM_PARALLEL_EXEC_UNITS, inter_op_parallelism_threads=2,
+                       allow_soft_placement=True, device_count={'CPU': NUM_PARALLEL_EXEC_UNITS})
+session = tf.Session(config=config)
+K.set_session(session)
+os.environ["OMP_NUM_THREADS"] = "4"
+os.environ["KMP_BLOCKTIME"] = "30"
+os.environ["KMP_SETTINGS"] = "1"
+os.environ["KMP_AFFINITY"] = "granularity=fine,verbose,compact,1,0"
