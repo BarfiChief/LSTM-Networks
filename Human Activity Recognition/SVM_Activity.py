e6f2f6365bceead84df080948ccaeb5e1dccf62c
Yashvardhan Singh <51024639+ys145@users.noreply.github.com>
2020-05-02 18:39:55 +0530

    Network with SVM as final classifier

---
+++ Human Activity Recognition/SVM_Activity.py
@@ -0,0 +1,130 @@
+from numpy import mean
+from numpy import std
+from numpy import dstack
+from pandas import read_csv
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import Dropout
+from keras.layers.recurrent import LSTM
+from keras.utils import to_categorical
+from keras.regularizers import l2
+from matplotlib import pyplot
+from matplotlib import pyplot as plt
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+import tensorflow as tf
+from keras import backend as K
+NUM_PARALLEL_EXEC_UNITS = 2
+config = tf.ConfigProto(intra_op_parallelism_threads=NUM_PARALLEL_EXEC_UNITS, inter_op_parallelism_threads=2,
+                       allow_soft_placement=True, device_count={'CPU': NUM_PARALLEL_EXEC_UNITS})
+session = tf.Session(config=config)
+K.set_session(session)
+os.environ["OMP_NUM_THREADS"] = "4"
+os.environ["KMP_BLOCKTIME"] = "30"
+os.environ["KMP_SETTINGS"] = "1"
+os.environ["KMP_AFFINITY"] = "granularity=fine,verbose,compact,1,0"
+
+# load a single file as a numpy array
+def load_file(filepath):
+  dataframe = read_csv(filepath, header=None, delim_whitespace=True)
+  return dataframe.values
+
+# load a list of files and return as a 3d numpy arrays
+def load_group(filenames, prefix=''):
+  loaded = list()
+  for name in filenames:
+    data = load_file(prefix + name)
+    loaded.append(data)
+  # stack group so that features are the 3rd dimension
+  loaded = dstack(loaded)
+  return loaded
+
+# load a dataset group, such as train or test
+def load_dataset_group(group, prefix=''):
+  filepath = prefix + group + '/Inertial Signals/'
+  # load all 9 files as a single array
+  filenames = list()
+  # total acceleration
+  filenames += ['total_acc_x_'+group+'.txt', 'total_acc_y_'+group+'.txt', 'total_acc_z_'+group+'.txt']
+  # body acceleration
+  filenames += ['body_acc_x_'+group+'.txt', 'body_acc_y_'+group+'.txt', 'body_acc_z_'+group+'.txt']
+  # body gyroscope
+  filenames += ['body_gyro_x_'+group+'.txt', 'body_gyro_y_'+group+'.txt', 'body_gyro_z_'+group+'.txt']
+  # load input data
+  X = load_group(filenames, filepath)
+  # load class output
+  y = load_file(prefix + group + '/y_'+group+'.txt')
+  return X, y
+
+# load the dataset, returns train and test X and y elements
+def load_dataset(prefix=''):
+  # load all train
+  trainX, trainy = load_dataset_group('train', prefix + '/Users/yashvardhansingh/Desktop/LSTM Results/Human Activity Recognition/Data/')
+  print(trainX.shape, trainy.shape)
+  # load all test
+  testX, testy = load_dataset_group('test', prefix + '/Users/yashvardhansingh/Desktop/LSTM Results/Human Activity Recognition/Data/')
+  print(testX.shape, testy.shape)
+  # zero-offset class values
+  trainy = trainy - 1
+  testy = testy - 1
+  # one hot encode y
+  trainy = to_categorical(trainy)
+  testy = to_categorical(testy)
+  print(trainX.shape, trainy.shape, testX.shape, testy.shape)
+  return trainX, trainy, testX, testy
+
+# fit and evaluate a model
+def evaluate_model(trainX, trainy, testX, testy):
+  verbose, epochs, batch_size = 1, 5, 100
+  n_timesteps, n_features, n_outputs = trainX.shape[1], trainX.shape[2], trainy.shape[1]
+  model = Sequential()
+  model.add(LSTM(100, input_shape=(n_timesteps,n_features)))
+  model.add(Dropout(0.5))
+  model.add(Dense(100, activation='relu'))
+  model.add(Dense(n_outputs, activation='linear',W_regularizer=l2(0.01)))
+  model.compile(loss='categorical_hinge', optimizer='adam', metrics=['accuracy'])
+  # fit network
+  history=model.fit(trainX, trainy, epochs=epochs, batch_size=batch_size,validation_split=0.2, verbose=verbose)
+  # evaluate model
+  _, accuracy = model.evaluate(testX, testy, batch_size=batch_size, verbose=0)
+  # Plot training & validation accuracy values
+  plt.plot(history.history['acc'])
+  plt.plot(history.history['val_acc'])
+  plt.title('Model Accuracy')
+  plt.ylabel('Accuracy')
+  plt.xlabel('Epochs')
+  plt.legend(['Training', 'Validation'], loc='upper left')
+  plt.show()
+  # Plot training & validation loss values
+  plt.plot(history.history['loss'])
+  plt.plot(history.history['val_loss'])
+  plt.title('Model Loss')
+  plt.ylabel('Loss')
+  plt.xlabel('Epochs')
+  plt.legend(['Training', 'Validation'], loc='upper right')
+  plt.show()
+  return accuracy
+# summarize scores
+def summarize_results(scores):
+  print(scores)
+  m, s = mean(scores), std(scores)
+  print('Accuracy: %.3f%% (+/-%.3f)' % (m, s))
+ 
+# run an experiment
+def run_experiment(repeats=1):
+  # load data
+  trainX, trainy, testX, testy = load_dataset()
+  # repeat experiment
+  scores = list()
+  for r in range(repeats):
+    score = evaluate_model(trainX, trainy, testX, testy)
+    score = score * 100.0
+    print('>#%d: %.3f' % (r+1, score))
+    scores.append(score)
+  # summarize results
+  summarize_results(scores)
+ 
+# run the experiment
+run_experiment()
